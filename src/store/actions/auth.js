9d092f986437bf59da9412dd1ee4e3f300931a99
lukasz.welnicki@gmail.com <lukaszwelnicki>
2019-04-23 22:13:37 +0200

    Auto signup

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -24,8 +24,29 @@ export const authFail = (error) => {
 };
 
 export const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationDate');
+    localStorage.removeItem('userId');
     return {
         type: actionTypes.AUTH_LOGOUT
+    }
+};
+
+export const authCheckState = () => {
+    return dispatch => {
+        const token = localStorage.getItem('token');
+        if (!token) {
+            dispatch(logout());
+        } else {
+            const expDate = new Date(localStorage.getItem('expirationDate'));
+            if (expDate > new Date()) {
+                const userId = localStorage.getItem('userId');
+                dispatch(authSuccess(token, userId));
+                dispatch(checkAuthTimeout((expDate.getTime() - new Date().getTime()) / 1000));
+            } else {
+                dispatch(logout());
+            }
+        }
     }
 };
 
@@ -52,6 +73,10 @@ export const auth = (email, password, isSignup) => {
         axios.post(url, authData)
             .then(response => {
                 console.log(response);
+                const expDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
+                localStorage.setItem('token', response.data.idToken);
+                localStorage.setItem('expirationDate', expDate);
+                localStorage.setItem('userId', response.data.localId);
                 dispatch(authSuccess(response.data.idToken, response.data.localId));
                 dispatch(checkAuthTimeout(response.data.expiresIn));
             })
